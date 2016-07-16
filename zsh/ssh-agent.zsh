419e1d7a53ef50f2efbdded64d88201fd7802266
David Dias <david.torresdias@gmail.com>
2016-07-16 12:10:49 +0200

    fix error on ssh-agent

--- zsh/ssh-agent.zsh
+++ zsh/ssh-agent.zsh
@@ -1,7 +1,10 @@
 if ! pgrep -u $USER ssh-agent > /dev/null; then
-    ssh-agent > ~/.ssh-agent-thing
+  if [ -f ~/.ssh-agent-thing ]; then
+    rm -f ~/.ssh-agent-thing 
+  fi
+  ssh-agent > ~/.ssh-agent-thing
 fi
 if [[ "$SSH_AGENT_PID" == "" ]]; then
-    eval $(<~/.ssh-agent-thing)
+  eval $(<~/.ssh-agent-thing)
 fi
 ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
